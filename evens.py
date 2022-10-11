930dfdcb0da20aa1b252deffba75c0cdc8a6ecfa
Henry Leon <henryatusabes@gmail.com>
2022-10-11 18:05:14 +0000

    Add final step in the Red-Green-Refactor cycle

--- evens.py
+++ evens.py
@@ -6,16 +6,17 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
-
-
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            return True
+        evens = sum([1 for n in numbers if n % 2 == 0])
 
+        return True if evens and evens % 2 == 0 else False
+                
+        # if evens:
+        #    return evens % 2 == 0
+        # else:
+        #    return False
     else:
         raise TypeError("A list was not passed into the function")
-
+        
 if __name__ == '__main__':
-    print(even_number_of_evens(5))
+    even_number_of_evens([2, 1, 4])
